e545f089e2586dd9caa6607cdf354b7ade6b311e
ankitkpro <ankitg@khiladipro.com>
2025-11-17 16:30:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ GCS_BUCKET_NAME = "qr-ai"
 GCS_PARENT_FOLDER_NAME = "cricket_batting/"
 # SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
 SERVICE_ACCOUNT_FILE_JSON = st.secrets["SERVICE_ACCOUNT_FILE"]
-# st.write(SERVICE_ACCOUNT_FILE_JSON)
+st.write(type(SERVICE_ACCOUNT_FILE_JSON))
 # SERVICE_ACCOUNT_FILE = json.loads(SERVICE_ACCOUNT_FILE_JSON)
 SERVICE_ACCOUNT_FILE = SERVICE_ACCOUNT_FILE_JSON
 
@@ -795,6 +795,7 @@ with st.sidebar:
         st.session_state.task_status = {}
         st.success("Cache cleared!")
         st.rerun()
+
 
 
 
