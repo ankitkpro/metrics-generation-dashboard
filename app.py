ed9919e8dcedf13a32226c61f4fccbc77ce9b72d
ankitkpro <ankitg@khiladipro.com>
2025-11-17 16:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ SERVICE_ACCOUNT_FILE_JSON = st.secrets["SERVICE_ACCOUNT_FILE"]
 # st.write(type(SERVICE_ACCOUNT_FILE_JSON))
 # SERVICE_ACCOUNT_FILE = json.loads(SERVICE_ACCOUNT_FILE_JSON)
 SERVICE_ACCOUNT_FILE = SERVICE_ACCOUNT_FILE_JSON
-st.write(SERVICE_ACCOUNT_FILE.keys())
+# st.write(SERVICE_ACCOUNT_FILE.keys())
 
 
 # BASE_URL = os.getenv("FLYTE_API_BASE_URL")
@@ -796,6 +796,7 @@ with st.sidebar:
         st.session_state.task_status = {}
         st.success("Cache cleared!")
         st.rerun()
+
 
 
 
